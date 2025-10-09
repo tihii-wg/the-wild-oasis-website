0a8c00cec674b001f3010d576fb84ffa3dff559f
tihii-wg <p0d0lian@mail.ru>
2025-10-09 22:48:16 +0300

    build suspence fetch cabins

--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -2,8 +2,9 @@ import Spinner from "@/app/_components/Spinner";
 
 export default function Loading() {
   return (
-    <div className="grid items-center content-center">
+    <div className="grid justify-center items-center">
       <Spinner />
+
       <p className="text-xl text-primary-200">Loading cabins data...</p>
     </div>
   );
