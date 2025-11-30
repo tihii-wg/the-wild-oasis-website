28df9765fca8831f3ac65b959f714542fd161161
tihii-wg <p0d0lian@mail.ru>
2025-11-30 20:34:09 +0200

    add loading indicator when updating form

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useFormStatus } from "react-dom";
 import { updateProfileForm } from "../_lib/actions";
 
 export default function UpdateProfileForm({ guest, children }) {
@@ -53,10 +54,20 @@ export default function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating ... " : "Update profile"}
+    </button>
   );
 }
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,20 +3,23 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { fi } from "date-fns/locale";
+import { getGuest } from "./data-service";
 
 export async function updateProfileForm(formData) {
   const session = await auth();
+  const { guestNationalID, guestNationality } = getGuest(session.user.id);
   if (!session) throw new Error("Need to be autoriser user");
 
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
-  console.log(session);
+
   if (!/^[A-Za-z0-9]{6,12}$/.test(nationalID))
     throw new Error("Enter valid National Id number");
+  
 
   const updatedData = { nationalID, nationality, countryFlag };
 
-  
   const { data, error } = await supabase
     .from("guests")
     .update(updatedData)
